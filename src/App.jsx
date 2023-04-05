aec4311222f07efc9954ea98c1467a2db3e6b8c8
yogisoutlooked <129572855+yogisoutlooked@users.noreply.github.com>
2023-04-05 22:25:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import Task from "./Task";
 import "./App.css";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Booking from "./Booking";
 
 
 function App() {
   return (
-    <div className="mt-0 max-w-lg font-poppins">
+    <div className="mt-0 max-w-lg font-poppins ">
       <Navbar />
+      <Booking />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
+      <Task />
       <Task />
       <Task />
       <Footer/>
