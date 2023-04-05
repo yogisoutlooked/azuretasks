0220089adb4aec830c40f25d3bf9c3ffdbf33b23
yogisoutlooked <129572855+yogisoutlooked@users.noreply.github.com>
2023-04-05 22:30:39 +0530

    Add files via upload

---
+++ src/Booking.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Booking() {
+  return (
+    <div className="font-bold font-poppins text-[10px] text-[#065BC9] ml-2 ">
+    Open Booking: 12
+  </div>
+  )
+}
+
+export default Booking
