cb866ae6a620b6a15f8e9d949eed6bc2b1516c92
yogisoutlooked <129572855+yogisoutlooked@users.noreply.github.com>
2023-04-05 22:22:20 +0530

    Update Task.jsx

--- src/Task.jsx
+++ src/Task.jsx
@@ -13,14 +13,7 @@ function task() {
 
   return (
     <div className="px-2 ">
-      <div className="font-bold font-poppins text-[10px] text-[#065BC9] ml-2 ">
-        Open Booking: 12
-      </div>
-
-      {/*..1 .............................................. */}
-
-      <Box sx={{ height: "150px", marginTop: 1, width: "100%" }}>
-        
+      <Box sx={{ height: "150px", marginTop: 1, width: "100%" }}>        
         <Card
           elevation={4}
           sx={{ borderRadius: "16px", height: "130px", textAlign: "left" }}>
@@ -28,21 +21,17 @@ function task() {
           <div className="m-3 w-[330px] absolute " >
             <Grid display="flex" container rowSpacing={1}>
               <Grid item xs={9} md={9}>
-                
                 <div className="font-poppins mb-1 text-[17px] font-medium text-[#28333c]">
                    {data.client}
                 </div>
-
                 <div className="font-poppins text-[13px] text-[#28333c] font-semibold">
                   <b>Model:</b>
                   {' '}{data.model}
                 </div>
-
                 <div className="font-poppins text-[13px] text-[#28333c] font-semibold my-1">
                   <b>Vin:</b>
                   {' '}{data.vin}
                 </div>
-
               </Grid>
               <Grid item xs={3} md={3}>
                 <div className="font-poppins font-semibold text-[10px] text-[#28333c] text-end">
@@ -50,7 +39,6 @@ function task() {
                 </div>
               </Grid>
             </Grid>
-
             <div className="w-90 h-3 mt-2 bg-gray-200 rounded-full dark:bg-gray-700 ">
               <div
                 className="bg-[#7BB564] text-[8px] font-bold h-3 text-white text-right align-centre  leading-none rounded-full"
@@ -60,7 +48,6 @@ function task() {
                 <span className="inline-block align-middle">75%</span>
               </div>
             </div>
-
             <div className=" flex justify-center text-[#28333c] h-1">
               <KeyboardArrowDownIcon   />
             </div>
@@ -68,166 +55,6 @@ function task() {
           </div> 
         </Card>
       </Box>
-
- {/* ..2.............................................. */}
-
-      <Box sx={{ height: "150px", marginTop: 1, width: "100%" }}>
-        
-        <Card
-          elevation={4}
-          sx={{ borderRadius: "16px", height: "130px", textAlign: "left" }}>
-            <div className=" bg-[#F2F3F8] relative h-60 w-[140px] rounded-r-[80px]">
-          <div className="m-3 w-[330px] absolute " >
-            <Grid display="flex" container rowSpacing={1}>
-              <Grid item xs={9} md={9}>
-                
-                <div className="font-poppins mb-1 text-[17px] font-medium text-[#28333c]">
-                   {data.client}
-                </div>
-
-                <div className="font-poppins text-[13px] text-[#28333c] font-semibold">
-                  <b>Model:</b>
-                  {' '}{data.model}
-                </div>
-
-                <div className="font-poppins text-[13px] text-[#28333c] font-semibold my-1">
-                  <b>Vin:</b>
-                  {' '}{data.vin}
-                </div>
-
-              </Grid>
-              <Grid item xs={3} md={3}>
-                <div className="font-poppins font-semibold text-[10px] text-[#28333c] text-end">
-                  Booking Date {data.booking}
-                </div>
-              </Grid>
-            </Grid>
-
-            <div className="w-90 h-3 mt-2 bg-gray-200 rounded-full dark:bg-gray-700 ">
-              <div
-                className="bg-[#FFA91F] text-[8px] font-bold h-3 text-white text-right align-centre  leading-none rounded-full"
-                Style="width: 45% " 
-              >
-                {" "}
-                <span className="inline-block align-middle">75%</span>
-              </div>
-            </div>
-
-            <div className=" flex justify-center text-[#28333c] h-1">
-              <KeyboardArrowDownIcon   />
-            </div>
-          </div>
-          </div> 
-        </Card>
-      </Box>
-
- {/* ....3.......................................... */}
-
- <Box sx={{ height: "150px", marginTop: 1, width: "100%" }}>
-        
-        <Card
-          elevation={4}
-          sx={{ borderRadius: "16px", height: "130px", textAlign: "left" }}>
-            <div className=" bg-[#F2F3F8] relative h-60 w-[140px] rounded-r-[80px]">
-          <div className="m-3 w-[330px] absolute " >
-            <Grid display="flex" container rowSpacing={1}>
-              <Grid item xs={9} md={9}>
-                
-                <div className="font-poppins mb-1 text-[17px] font-medium text-[#28333c]">
-                   {data.client}
-                </div>
-
-                <div className="font-poppins text-[13px] text-[#28333c] font-semibold">
-                  <b>Model:</b>
-                  {' '}{data.model}
-                </div>
-
-                <div className="font-poppins text-[13px] text-[#28333c] font-semibold my-1">
-                  <b>Vin:</b>
-                  {' '}{data.vin}
-                </div>
-
-              </Grid>
-              <Grid item xs={3} md={3}>
-                <div className="font-poppins font-semibold text-[10px] text-[#28333c] text-end">
-                  Booking Date {data.booking}
-                </div>
-              </Grid>
-            </Grid>
-
-            <div className="w-90 h-3 mt-2 bg-gray-200 rounded-full dark:bg-gray-700 ">
-              <div
-                className="bg-[#FF0000] text-[8px] font-bold h-3 text-white text-right align-centre  leading-none rounded-full"
-                Style="width: 45% " 
-              >
-                {" "}
-                <span className="inline-block align-middle">75%</span>
-              </div>
-            </div>
-
-            <div className=" flex justify-center text-[#28333c] h-1">
-              <KeyboardArrowDownIcon   />
-            </div>
-          </div>
-          </div> 
-        </Card>
-      </Box>
-
- {/* .....4......................................... */}
-
-
-
- <Box sx={{ height: "150px", marginTop: 1, width: "100%" }}>
-        
-        <Card
-          elevation={4}
-          sx={{ borderRadius: "16px", height: "130px", textAlign: "left" }}>
-            <div className=" bg-[#F2F3F8] relative h-60 w-[140px] rounded-r-[80px]">
-          <div className="m-3 w-[330px] absolute " >
-            <Grid display="flex" container rowSpacing={1}>
-              <Grid item xs={9} md={9}>
-                
-                <div className="font-poppins mb-1 text-[17px] font-medium text-[#28333c]">
-                   {data.client}
-                </div>
-
-                <div className="font-poppins text-[13px] text-[#28333c] font-semibold">
-                  <b>Model:</b>
-                  {' '}{data.model}
-                </div>
-
-                <div className="font-poppins text-[13px] text-[#28333c] font-semibold my-1">
-                  <b>Vin:</b>
-                  {' '}{data.vin}
-                </div>
-
-              </Grid>
-              <Grid item xs={3} md={3}>
-                <div className="font-poppins font-semibold text-[10px] text-[#28333c] text-end">
-                  Booking Date {data.booking}
-                </div>
-              </Grid>
-            </Grid>
-
-            <div className="w-90 h-3 mt-2 bg-gray-200 rounded-full dark:bg-gray-700 ">
-              <div
-                className="bg-[#7BB564] text-[8px] font-bold h-3 text-white text-right align-centre  leading-none rounded-full"
-                Style="width: 45% " 
-              >
-                {" "}
-                <span className="inline-block align-middle">75%</span>
-              </div>
-            </div>
-
-            <div className=" flex justify-center text-[#28333c] h-1">
-              <KeyboardArrowDownIcon   />
-            </div>
-          </div>
-          </div> 
-        </Card>
-      </Box>
-
-
     </div>
   );
 }
